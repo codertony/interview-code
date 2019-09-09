52117746f9992b8347ee0e0aa55d6dd75a3199aa
codertony <932010806@qq.com>
2019-09-09 16:46:28 +0800

    测试getter的过程

--- src/store.js
+++ src/store.js
@@ -15,4 +15,37 @@ let $store = new Vuex.Store({
   }
 })
 
+/**
+ * @author tony
+ * @date 2019-09-05 09:45
+ * @name updataDebounce
+ * @Description 更新数据防抖
+ * @param {any} data - 需要更新的数据.
+ * @param {number} wait - 防抖时间.
+ * @return {promise} xxx.
+ */
+let debounceMap = new Map()
+$store.updataDebounce = function(data, wait = 100) {
+  return new Promise((resolve, reject) => {
+    let waitTimer = 0
+    if (debounceMap.has(data)) {
+      waitTimer = debounceMap.get(data)
+    }
+    waitTimer && clearTimeout(waitTimer)
+    waitTimer = setTimeout(() => {
+      data['$undefind$'] = {}
+      Vue.delete(data, '$undefind$')
+      // 相当于下面的代码
+      // let ob = data.__ob__;
+      // if(ob) {
+      //   ob.dep.notify()
+      // }
+      resolve(data)
+    }, wait)
+    debounceMap.set(data, waitTimer)
+  })
+}
+
+window.$store = $store
+
 export default $store
